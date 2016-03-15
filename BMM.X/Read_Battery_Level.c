46feac03179f6bbc31000ff53e01dc65ae087980
drummerboy2543 <att11@zips.uakron.edu>
2016-03-15 13:38:25 -0400

    Added the LTC6804 files from  Linear Tech. Started editing the functions to work in mplab and started making other files to interface with the LTC6804 source and header files to be used for the car.
    Andrew Thornborough

---
+++ BMM.X/Read_Battery_Level.c
@@ -0,0 +1,37 @@
+
+#include "Read_Battery_Level.h"
+
+void Start_LTC6804_initialize() {
+    LTC6804_initialize();
+}
+
+void Read_Battery(int BatteryPlacement) {
+
+    switch (BatteryPlacement) {
+        case 0: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_ALL, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 1: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_1and7, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 2: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_2and8, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 3: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_3and9, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 4: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_4and10, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 5: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_5and11, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        case 6: set_adc(MD_NORMAL, DCP_DISABLED, CELL_CH_6and12, AUX_CH_ALL);
+            LTC6804_adcv();
+            break;
+        default:
+            break;
+    }
+                
+            
+}
---
+++ BMM.X/Read_Battery_Level.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   Read_Battery_Level.h
+ * Author: Andrew
+ *
+ * Created on March 15, 2016, 12:53 PM
+ */
+#include"LT6804.h"
+#ifndef READ_BATTERY_LEVEL_H
+#define	READ_BATTERY_LEVEL_H
+
+void Start_LTC6804_initialize();
+void Read_Battery(int BatteryPlacement);
+
+
+            
+#endif	/* READ_BATTERY_LEVEL_H */
+
