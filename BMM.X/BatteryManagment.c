37f7b6c7712e70eaacc584cc55b7b4968909f3c8
richcj10 <richcj10@gmail.com>
2016-04-04 01:31:20 -0400

    Added fcn's for low level controls.

--- BMM.X/BatteryManagment.c
+++ BMM.X/BatteryManagment.c
@@ -1,6 +1,11 @@
 
 #include "BatteryManagment.h"
 
+//|r_config[0]|r_config[1]|r_config[2]|r_config[3]|r_config[4]|r_config[5]|r_config[6]  |r_config[7] |r_config[8]|r_config[9]|  .....    |
+//|-----------|-----------|-----------|-----------|-----------|-----------|-------------|------------|-----------|-----------|-----------|
+//|IC1 CFGR0  |IC1 CFGR1  |IC1 CFGR2  |IC1 CFGR3  |IC1 CFGR4  |IC1 CFGR5  |IC1 PEC High |IC1 PEC Low |IC2 CFGR0  |IC2 CFGR1  |  .....    |
+
+
 void Start_LTC6804_initialize() {
     LTC6804_initialize();
 }
@@ -34,4 +39,49 @@ void Read_Battery(int BatteryPlacement) {
     }
                 
             
+}
+
+void SetBypass(int bank, int ic, int cell, bool value){
+	if(value){
+		if(cell < 8){
+			CFGR4 = CFGR4 | (1 << cell);
+		}
+		else(){
+			CFGR5 = CFGR5 | (1 << (cell-8));
+		}
+	}
+	else(){
+		if(cell < 8){
+			CFGR4 = CFGR4 & ~(1 << cell);
+		}
+		else(){
+			CFGR5 = CFGR5 & ~(1 << (cell-8));
+		}
+	}
+	LTC6804_DATA[bank][ic][5] = CFGR5;
+	LTC6804_DATA[bank][ic][4] = CFGR4;
+}
+
+void SetTempEnable(int bank, int ic, bool value){
+	if(value){
+		CFGR0 = CFGR0 | (1 << 4);
+	}
+	else(){
+		CFGR0 = CFGR0 & ~(1 << 4);
+	}
+	LTC6804_DATA[bank][ic][0] = CFGR0;
+}
+
+void SetUnderOverVoltage(int under, int over){
+	int uv[2];
+	int ov[2]0;
+	
+	uv[0] = ((under >> 0) & 0xff);
+	uv[1] = ((under >> 8) & 0xff);
+	
+	ov[0] = ((over >> 12) & 0xff);
+	ov[1] = ((over >> 4) & 0xff);
+	
+	CFGR1 = uv[0];
+	CFGR2 = uv[1];
 }
--- BMM.X/BatteryManagment.h
+++ BMM.X/BatteryManagment.h
@@ -8,9 +8,21 @@
 #ifndef READ_BATTERY_LEVEL_H
 #define	READ_BATTERY_LEVEL_H
 
+#define NUMBEROFIC 12
+#define NUMBEROFCH 2
+#define NUMBEROFDATA 8
+
+LTC6804_DATA[NUMBEROFCH][NUMBEROFIC][NUMBEROFDATA];
+
 void Start_LTC6804_initialize();
 void Read_Battery(int BatteryPlacement);
 
+int CFGR0 = 0;
+int CFGR1 = 0;
+int CFGR2 = 0;
+int CFGR3 = 0;
+int CFGR4 = 0;
+int CFGR5 = 0;
 
             
 #endif	/* READ_BATTERY_LEVEL_H */
